c3723ffc46b8e2745e56eae71d7b966ee7d69a76
jhonsnow456 <thakuraman22july@gmail.com>
2021-09-12 15:08:46 +0530

    modified few files and done minor correction

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,31 @@
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
-
+import Homepage from "./pages/homepage/Homepage";
 import TopBar from "./components/topbar/TopBar";
-import Homepage from "./pages/homepage/homepage";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 function App() {
-  const user = false;
+  const { user } = useContext(Context);
   return (
     <Router>
-        <TopBar />
-        <Switch>
-          <Route exact path='/'>
-            <Homepage />
-          </Route>
-
-          <Route path='/register'> {user ? <Homepage /> : <Register /> } </Route>
-          <Route path='/login'> {user ? <Homepage /> : <Login /> } </Route>
-          <Route path='/settings'> {user ? <Settings /> : <Register /> } </Route>
-          <Route path='/write'> {user ? <Write /> : <Register /> } </Route> 
-
-          <Route path='/post/:postId'>
-            <Single />
-          </Route>
-        </Switch>       
+      <TopBar />
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="/register">{user ? <Homepage /> : <Register />}</Route>
+        <Route path="/login">{user ? <Homepage /> : <Login />}</Route>
+        <Route path="/write">{user ? <Write /> : <Register />}</Route>
+        <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
     </Router>
   );
 }
