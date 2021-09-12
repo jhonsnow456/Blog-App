a0a461250bdb959cadb0dfc89f0f6c286830defc
jhonsnow456 <thakuraman22july@gmail.com>
2021-09-12 15:06:20 +0530

    modified homepage

--- client/src/pages/single/Single.jsx
+++ client/src/pages/single/Single.jsx
@@ -1,13 +1,12 @@
-import SideBar from '../../components/sidebar/SideBar';
-import SinglePost from '../../components/singlePost/SinglePost';
-
 import "./single.css";
+import SideBar from "../../components/sidebar/SideBar";
+import SinglePost from "../../components/singlePost/SinglePost";
 
 export default function Single() {
-    return (
-        <div className="single">
-            <SinglePost />
-            <SideBar />
-        </div>
-    )
+  return (
+    <div className="single">
+      <SinglePost/>
+      <SideBar />
+    </div>
+  );
 }
